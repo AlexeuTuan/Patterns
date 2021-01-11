dadfb0d56f9d7d44f374ac3c3580339c2a084495
AlexeyTyan <alekseytyan45@gmail.com>
2021-01-11 15:16:57 +0300

    Template Method

--- src/alekseytyan/Main.java
+++ src/alekseytyan/Main.java
@@ -1,27 +1,7 @@
 package alekseytyan;
 
-import alekseytyan.adapter.duckadapter.Duck;
-import alekseytyan.adapter.duckadapter.MallardDuck;
-import alekseytyan.adapter.duckadapter.Turkey;
-import alekseytyan.adapter.duckadapter.TurkeyAdapter;
-import alekseytyan.adapter.iteratoradapter.EnumerationAdapter;
-
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<String> testList = new ArrayList<>();
-        testList.add("string one");
-        testList.add("string two");
-        testList.add("string three");
-        testList.add("string four");
 
-        Enumeration<String> enumeration = new EnumerationAdapter<>(testList.iterator());
-
-        while (enumeration.hasMoreElements()) {
-            System.out.println(enumeration.nextElement());
-        }
     }
 }
